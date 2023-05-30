fc3e00524c89f5138130a0fbf3d0a5cd4c4a14e4
glucogen19 <98939682+glucogen19@users.noreply.github.com>
2023-05-30 10:57:19 +0400

    6) Вывод переменной класса

--- m.cpp
+++ m.cpp
@@ -9,4 +9,7 @@ class TPen {
 int main(){
 	cout << "What color is the UN flag?" << endl;
     TPen pen;
+    pen.color = " #5b92e5";
+    cout << "Color of the UN flag =" << pen.color << endl;
+    return 0;
 }
